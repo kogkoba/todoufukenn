989d86247ab198ac7cf3fb032ca453b5573eb582
kogkoba <kc.koba@gmail.com>
2025-03-16 17:25:29 +0900

    Update script.js

--- games/karuta/script.js
+++ games/karuta/script.js
@@ -1,39 +1,42 @@
-```javascript
 let time = 60;
 let points = 0;
 let timer;
 let questions = [];
 
 async function loadQuestions() {
-    const res = await fetch('./data/questions.json');
+    const res = await fetch('questions.json');
     questions = await res.json();
 }
 
 function startGame() {
     document.getElementById("start-button").style.display = "none";
     document.getElementById("game-area").style.display = "block";
+    
     timer = setInterval(() => {
         time--;
         document.getElementById('timer').innerText = time;
         if (time <= 0) {
             clearInterval(timer);
-            alert("ゲーム終了！");
+            alert(`ゲーム終了！ 獲得ポイント: ${points}点`);
         }
     }, 1000);
+    
     nextQuestion();
 }
 
-async function nextQuestion() {
-    const question = questions[Math.floor(Math.random() * questions.length)];
-    document.getElementById('question').innerText = question.question;
+function nextQuestion() {
+    if (time <= 0) return;
+    
+    const questionData = questions[Math.floor(Math.random() * questions.length)];
+    document.getElementById('question').innerText = questionData.question;
     
     let choices = [...questions].sort(() => Math.random() - 0.5).slice(0, 5);
-    choices.push(question);
+    choices.push(questionData);
     choices = choices.sort(() => Math.random() - 0.5);
-    
+
     let cardsHTML = '';
     choices.forEach(pref => {
-        cardsHTML += `<img src="assets/images/${pref.answer}" onclick="checkAnswer('${pref.answer}', '${question.answer}')">`;
+        cardsHTML += `<img src="assets/images/${pref.answer}" onclick="checkAnswer('${pref.answer}', '${questionData.answer}')">`;
     });
     document.getElementById('cards').innerHTML = cardsHTML;
 }
@@ -50,6 +53,3 @@ function checkAnswer(selected, answer) {
 
 document.getElementById("start-button").addEventListener("click", startGame);
 window.onload = loadQuestions;
-```
-
----
